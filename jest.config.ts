bd12cd59f30e6054e25469f247de482eda074366
Hallyson10 <hallysonjean@hotmail.com>
2022-06-22 18:51:53 -0300

    ajust tests

--- jest.config.ts
+++ jest.config.ts
@@ -17,10 +17,10 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: true,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)'],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: undefined,
